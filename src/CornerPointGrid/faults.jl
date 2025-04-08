9abbf99b6292bb9244abc1828920ab7070264b69
Olav Møyner <olav.moyner@gmail.com>
2025-04-08 11:48:56 +0200

    Move fault processing into a separate function

--- src/CornerPointGrid/faults.jl
+++ src/CornerPointGrid/faults.jl
@@ -8,43 +8,54 @@ function mesh_add_fault_tags!(G::UnstructuredMesh, faults)
 
     sorted_faces = (sorted_face_pair_I, sorted_face_pair_J, sorted_face_pair_K)
     for (fault, specs) in faults
-        fault_faces = Int[]
-        for (I, J, K, dir) in specs
-            IJK = (I, J, K)
-            @assert length(dir) == 1 || length(dir) == 2
-            d = dir[1]
-            if d == 'X' || d == 'I'
-                ix_self = 1
-                ix_1 = 2
-                ix_2 = 3
-            elseif d == 'Y' || d == 'J'
-                ix_self = 2
-                ix_1 = 1
-                ix_2 = 3
-            elseif d == 'Z' || d == 'K'
-                ix_self = 3
-                ix_1 = 1
-                ix_2 = 2
-            else
-                error("Bad direction for fault $fault entry: $dir")
-            end
-            if length(dir) == 1 || dir[2] == '+'
-                inc = 1
-            else
-                @assert dir[2] == '-'
-                inc = -1
-            end
-            range_self = IJK[ix_self]
-            range_1 = IJK[ix_1]
-            range_2 = IJK[ix_2]
-            @assert length(range_self) == 1
-            self = range_self[1]
-            match_fault_to_faces!(fault_faces, range_1, sorted_faces[ix_1], range_2, sorted_faces[ix_2], self, sorted_faces[ix_self], inc)
-        end
+        fault_faces = fault_to_faces(G::UnstructuredMesh, specs, sorted_faces, fault)
         @debug "Fault $fault: Added $(length(fault_faces)) faces"
         set_mesh_entity_tag!(G, Faces(), :faults, Symbol(fault), fault_faces)
     end
     return G
+end
+
+function fault_to_faces(G::UnstructuredMesh, specs, sorted_faces = missing, faultname = :FAULT)
+    if ismissing(sorted_faces)
+        N = G.faces.neighbors
+        sorted_face_pair_I = get_sorted_face_pairs(N, ijk, 1)
+        sorted_face_pair_J = get_sorted_face_pairs(N, ijk, 2)
+        sorted_face_pair_K = get_sorted_face_pairs(N, ijk, 3)
+    end
+    fault_faces = Int[]
+    for (I, J, K, dir) in specs
+        IJK = (I, J, K)
+        @assert length(dir) == 1 || length(dir) == 2
+        d = dir[1]
+        if d == 'X' || d == 'I'
+            ix_self = 1
+            ix_1 = 2
+            ix_2 = 3
+        elseif d == 'Y' || d == 'J'
+            ix_self = 2
+            ix_1 = 1
+            ix_2 = 3
+        elseif d == 'Z' || d == 'K'
+            ix_self = 3
+            ix_1 = 1
+            ix_2 = 2
+        else
+            error("Bad direction for fault $fault entry: $dir")
+        end
+        if length(dir) == 1 || dir[2] == '+'
+            inc = 1
+        else
+            @assert dir[2] == '-'
+            inc = -1
+        end
+        range_self = IJK[ix_self]
+        range_1 = IJK[ix_1]
+        range_2 = IJK[ix_2]
+        @assert length(range_self) == 1
+        self = range_self[1]
+        match_fault_to_faces!(fault_faces, range_1, sorted_faces[ix_1], range_2, sorted_faces[ix_2], self, sorted_faces[ix_self], inc)
+    end
+    return fault_faces
 end
 
 function mesh_add_fault_tags!(G::UnstructuredMesh, faults::Missing)

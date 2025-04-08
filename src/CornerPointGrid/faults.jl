76f7ac80fe1e795c74a7e1f711838e461805e89b
Olav Møyner <olav.moyner@gmail.com>
2025-04-08 15:26:58 +0200

    Improve fault tagging in presence of strong pinch

--- src/CornerPointGrid/faults.jl
+++ src/CornerPointGrid/faults.jl
@@ -1,26 +1,41 @@
 function mesh_add_fault_tags!(G::UnstructuredMesh, faults)
-    sorted_faces = get_sorted_face_pairs(G)
+    lookups = get_ijk_lookups(G)
     for (fault, specs) in faults
-        fault_faces = fault_to_faces(G::UnstructuredMesh, specs, sorted_faces, fault)
+        fault_faces = fault_to_faces(G::UnstructuredMesh, specs, lookups, fault)
         @debug "Fault $fault: Added $(length(fault_faces)) faces"
         set_mesh_entity_tag!(G, Faces(), :faults, Symbol(fault), fault_faces)
     end
     return G
 end
 
-function get_sorted_face_pairs(G::UnstructuredMesh)
-    ijk = map(x -> cell_ijk(G, x), 1:number_of_cells(G))
-    N = G.faces.neighbors
-    sorted_face_pair_I = get_sorted_face_pairs(N, ijk, 1)
-    sorted_face_pair_J = get_sorted_face_pairs(N, ijk, 2)
-    sorted_face_pair_K = get_sorted_face_pairs(N, ijk, 3)
-    sorted_faces = (sorted_face_pair_I, sorted_face_pair_J, sorted_face_pair_K)
-    return sorted_faces
+function build_ijk_lookup(all_ijk, dim, cartdims)
+    @assert dim in (1, 2, 3)
+    lookup = Dict{Int, Vector{Int}}()
+    for i in 1:cartdims[dim]
+        lookup[i] = Int[]
+    end
+    for (cell, ijk) in enumerate(all_ijk)
+        push!(lookup[ijk[dim]], cell)
+    end
+    for i in 1:cartdims[dim]
+        lookup[i] = sort(lookup[i])
+    end
+    return lookup
 end
 
-function fault_to_faces(G::UnstructuredMesh, specs, sorted_faces = missing, faultname = :FAULT)
-    if ismissing(sorted_faces)
-        sorted_faces = get_sorted_face_pairs(G)
+function get_ijk_lookups(G::UnstructuredMesh)
+    ijk = map(x -> cell_ijk(G, x), 1:number_of_cells(G))
+    cartdims = G.structure.I
+    getl(dim) = build_ijk_lookup(ijk, dim, cartdims)
+    lookups = (I = getl(1), J = getl(2), K = getl(3))
+    return (lookups, ijk)
+end
+
+function fault_to_faces(G::UnstructuredMesh, specs, lookups = missing, faultname = :FAULT)
+    if ismissing(lookups)
+        ijk_lookups, ijk_cells = get_ijk_lookups(G)
+    else
+        ijk_lookups, ijk_cells = lookups
     end
     fault_faces = Int[]
     for (I, J, K, dir) in specs
@@ -52,8 +67,10 @@ function fault_to_faces(G::UnstructuredMesh, specs, sorted_faces = missing, faul
         range_1 = IJK[ix_1]
         range_2 = IJK[ix_2]
         @assert length(range_self) == 1
-        self = range_self[1]
-        match_fault_to_faces!(fault_faces, range_1, sorted_faces[ix_1], range_2, sorted_faces[ix_2], self, sorted_faces[ix_self], inc)
+        self = only(range_self)
+        self_ijk_lookup = ijk_lookups[ix_self][self]
+        other_ijk_lookup = ijk_lookups[ix_self][self+inc]
+        match_fault_to_faces!(fault_faces, G, ijk_cells, range_1, range_2, ix_1, ix_2, self_ijk_lookup, other_ijk_lookup)
     end
     return fault_faces
 end
@@ -85,16 +102,24 @@ function sorted_neighbor_tuple(a, b)
     end
 end
 
-function match_fault_to_faces!(fault_faces, range_1, sorted_faces_1, range_2, sorted_faces_2, self, sorted_faces_self, inc)
-    pair = sorted_neighbor_tuple(self, self+inc)
-    rng = searchsorted(sorted_faces_self.N_sorted, pair)
-    faces = view(sorted_faces_self.faces_sorted, rng)
-    for face in faces
-        l_1, r_1 = sorted_faces_1.N[face]
-        if l_1 in range_1 && r_1 in range_1
-            l_2, r_2 = sorted_faces_2.N[face]
-            if l_2 in range_2 && r_2 in range_2
-                push!(fault_faces, face)
+function match_fault_to_faces!(fault_faces, G, ijk, range_1, range_2, ix_1, ix_2, self_cells, other_cells)
+    function cell_in_ranges(c)
+        ijk_c = ijk[c]
+        return ijk_c[ix_1] in range_1 && ijk_c[ix_2] in range_2
+    end
+    n_other = length(other_cells)
+    for cell in self_cells
+        if cell_in_ranges(cell)
+            for f in G.faces.cells_to_faces[cell]
+                l, r = G.faces.neighbors[f]
+                if l == cell
+                    other_cell = r
+                else
+                    other_cell = l
+                end
+                if insorted(other_cell, other_cells)
+                    push!(fault_faces, f)
+                end
             end
         end
     end

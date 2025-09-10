912de2e380a3600ed6e56bad4bac3958463f30ae
Olav Møyner <olav.moyner@gmail.com>
2025-09-10 21:35:50 +0200

    Update IXParser.jl

--- src/IXParser/IXParser.jl
+++ src/IXParser/IXParser.jl
@@ -9,10 +9,9 @@ module IXParser
     import Jutul: si_unit
     using GeoEnergyIO
 
-    """
-    Well completion flags
-    """
+    # Well completion flags
     @enum IX_WELLSTATUS IX_OPEN IX_CLOSED IX_HEAT
+    # IJK indices
     @enum IX_IJK IX_I IX_J IX_K
 
     include("types.jl")

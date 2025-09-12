657e384bc0b2e43fb6e65b4ff663010efc681c16
Olav Møyner <olav.moyner@gmail.com>
2025-09-12 10:46:52 +0200

    Add obsh to docs + reformatter

--- src/IXParser/utils.jl
+++ src/IXParser/utils.jl
@@ -24,7 +24,15 @@ function strip_empty_strings(s::AbstractString)
     return replace(s, " \"\" " => "NONE")
 end
 
-function read_obsh_file(pth)
+"""
+    obs_data = read_obs_file(pth)
+    obs_data = read_obs_file(pth, reformat = true)
+
+Read an OBSH file from path `pth`. If `reformat` is true, the data is
+reformatted into a dictionary of wells, each containing a dictionary of
+data series and a vector of `DateTime` objects.
+"""
+function read_obsh_file(pth; reformat = false)
     h = open(pth)
     units = readline(h)
     date_format = readline(h)
@@ -48,11 +56,66 @@ function read_obsh_file(pth)
         end
     end
     close(h)
-    return Dict(
+    out = Dict(
         "units" => units,
         "date_format" => date_format,
         "header" => new_header,
         "data" => data,
     )
+    if reformat
+        out = reformat_obsh_file(out)
+    end
+    return out
 end
 
+function reformat_obsh_file(x::AbstractDict)
+    months = Dict(
+        "jan" => 1,
+        "feb" => 2,
+        "mar" => 3,
+        "apr" => 4,
+        "may" => 5,
+        "jun" => 6,
+        "jul" => 7,
+        "aug" => 8,
+        "sep" => 9,
+        "oct" => 10,
+        "nov" => 11,
+        "dec" => 12
+    )
+    header = x["header"]
+    function header_index(key)
+        ix = findfirst(isequal(key), header)
+        @assert !isnothing(ix) "Did not find header $key in obsh file header $header"
+        return ix
+    end
+    wellix = header_index("WELL")
+    dayix = header_index("DAY")
+    monthix = header_index("MONTH")
+    yearix = header_index("YEAR")
+    remainder = setdiff(header, ["WELL", "DAY", "MONTH", "YEAR"])
+    otherix = [header_index(h) for h in remainder]
+    data = x["data"]
+
+    out = Dict{String, Any}()
+    for row in axes(data, 1)
+        well = data[row, wellix]
+        if !haskey(out, well)
+            out[well] = Dict{String, Any}()
+            for h in remainder
+                out[well][h] = Float64[]
+            end
+            out[well]["dates"] = DateTime[]
+        end
+        welldest = out[well]
+        day = data[row, dayix]
+        month = months[lowercase(data[row, monthix])[1:3]]
+        year = data[row, yearix]
+        date = DateTime(year, month, day)
+        push!(welldest["dates"], date)
+        for (i, h) in enumerate(remainder)
+            push!(welldest[h], data[row, otherix[i]])
+        end
+    end
+    return Dict("wells" => out, "keys" => remainder)
+end

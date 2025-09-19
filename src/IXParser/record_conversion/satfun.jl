6e5236620861b79a577f82ff7535d111c6519614
Olav Møyner <olav.moyner@gmail.com>
2025-09-19 15:15:32 +0200

    Rework saturation fun parsing

--- src/IXParser/record_conversion/satfun.jl
+++ src/IXParser/record_conversion/satfun.jl
@@ -1,3 +1,25 @@
 function convert_ix_record(x, unit_systems, meta, ::Val{:SaturationFunction})
-    return parse_and_convert_numerical_table(x, unit_systems, "SaturationFunction")
+    to_convert = []
+    out = Dict{String, Any}()
+    for rec in x.body
+        if rec isa IXEqualRecord
+            out[rec.keyword] = rec.value
+        else
+            rec::IXStandardRecord
+            kw = rec.keyword
+            if !haskey(out, kw)
+                out[kw] = Dict{String, Any}()
+            end
+            group = rec.value
+            if !haskey(out[kw], group)
+                out[kw][group] = Dict{String, Any}()
+            end
+            set_ix_array_values!(out[kw][group], rec.body, T = Float64)
+            push!(to_convert, (kw, group))
+        end
+    end
+    for (kw, group) in to_convert
+        convert_dict_entries!(out[kw][group], unit_systems)
+    end
+    return out
 end

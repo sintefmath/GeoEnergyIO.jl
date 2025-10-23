37cca76c611752c2c26de7dedb831bb67d3a3e10
Olav Møyner <olav.moyner@gmail.com>
2025-10-23 12:07:55 +0200

    Reorder type defs

--- src/IXParser/types.jl
+++ src/IXParser/types.jl
@@ -18,16 +18,6 @@ struct IXEqualRecord <: AbstractIXRecord
     value::Any
 end
 
-struct IXAssignmentRecord <: AbstractIXRecord
-    keyword::String
-    index::Union{Int, String}
-    value::Any
-end
-
-function IXAssignmentRecord(kw::Base.String, index::IXKeyword, value)
-    return IXAssignmentRecord(kw, String(index), value)
-end
-
 function Base.show(io::IO, t::MIME"text/plain", dopt::IXEqualRecord)
     println(io, "IXEqualRecord: $(dopt.keyword) = $(dopt.value)")
 end
@@ -38,6 +28,16 @@ end
 
 Base.String(x::IXKeyword) = x.keyword
 Base.convert(::Type{String}, x::IXKeyword) = x.keyword
+
+struct IXAssignmentRecord <: AbstractIXRecord
+    keyword::String
+    index::Union{Int, String}
+    value::Any
+end
+
+function IXAssignmentRecord(kw::Base.String, index::IXKeyword, value)
+    return IXAssignmentRecord(kw, String(index), value)
+end
 
 struct IXFunctionCall <: AbstractIXRecord
     keyword::String

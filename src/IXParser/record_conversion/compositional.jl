8af69f863371fa2d59898485a61fefe75a845d38
Olav Møyner <olav.moyner@gmail.com>
2025-11-09 21:16:00 +0100

    Add std temp/p

--- src/IXParser/record_conversion/compositional.jl
+++ src/IXParser/record_conversion/compositional.jl
@@ -4,3 +4,11 @@ function convert_ix_record(x, unit_systems, meta, ::Val{:ComponentProperties})
     convert_dict_entries!(tab, unit_systems)
     return tab
 end
+
+function convert_ix_record(x::IXEqualRecord, unit_systems, meta, ::Val{:StandardPressure})
+    return swap_unit_system(x.value, unit_systems, :pressure)
+end
+
+function convert_ix_record(x::IXEqualRecord, unit_systems, meta, ::Val{:StandardTemperature})
+    return swap_unit_system(x.value, unit_systems, :relative_temperature)
+end

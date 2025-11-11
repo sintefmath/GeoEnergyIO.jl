4bc956d2778695c46255812524d184ebd8aa56b1
Olav Møyner <olav.moyner@gmail.com>
2025-11-11 21:12:46 +0100

    Update meta.jl

--- src/IXParser/record_conversion/meta.jl
+++ src/IXParser/record_conversion/meta.jl
@@ -1,6 +1,11 @@
 function convert_ix_record(x::IXEqualRecord, unit_systems, meta, ::Val{:StructuredInfoMgr})
+    if length(x.value) == 0
+        uuid = missing
+    else
+        uuid = only(x.value).value
+    end
     return Dict(
         "name" => x.keyword,
-        "UUID" => only(x.value).value
+        "UUID" => uuid
     )
 end

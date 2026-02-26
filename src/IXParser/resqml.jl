84efbfb97a5e7cd87f5ea7dcb447d83f0c61943b
Olav Møyner <olav.moyner@gmail.com>
2026-02-26 15:15:13 +0100

    Update resqml.jl

--- src/IXParser/resqml.jl
+++ src/IXParser/resqml.jl
@@ -71,7 +71,13 @@ function convert_resqml_props(r, unit_systems = missing; verbose = false, strict
         out["title"] = find_string_by_tag(prop, "eml20:Title")
         out["unit"] = find_string_by_tag(prop, "resqml20:UOM")
         # Are values always patch0?
-        read_obj = HDF5.read(r.h5["/RESQML/$uuid_obj"])
+        @show r.h5
+
+        k = "/RESQML/$uuid_obj"
+        if !haskey(r.h5, k)
+            k = "/resqml20/$uuid_obj"
+        end
+        read_obj = HDF5.read(r.h5[k])
         if length(keys(read_obj)) == 1
             read_obj = only(values(read_obj))
             if is_continuous
